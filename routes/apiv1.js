6883cd0049da1e0ecdd83c3f3650a8c193f426b4
Venkata Ratnam Alubelli <valubell@in.ibm.com>
2020-07-20 11:18:31 +0000

    Added comment

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -38,7 +38,7 @@ exports.getWeather = function(req, res) {
 
 };
 router.get('/getWeather', exports.getWeather);
-
+/*
 exports.getWeather2 = function(req, res) {
 	var zip = req.query.zip;
 	if( (zip === null) || (typeof(zip) === 'undefined') ) {
@@ -68,7 +68,7 @@ exports.getWeather2 = function(req, res) {
 
 };
 router.get('/getWeather2', exports.getWeather2);
-
+*/
 exports.getWeather3 = function(req, res) {
 	var zip = req.query.zip;
 	if( (zip === null) || (typeof(zip) === 'undefined') ) {
